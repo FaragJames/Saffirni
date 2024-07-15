70ddf6007cf2ce0215f9c8d27fb1f3ab39660bbb
rorno <rornoozoro@gmail.com>
2024-07-15 13:44:50 +0300

    moiad

--- src/App.tsx
+++ src/App.tsx
@@ -115,6 +115,7 @@ export default function App() {
                 <>
                     <Navbar />
                     <SearchForm isHomePage={true} />
+                    <Main/>
                     <Footer />
                 </>
             ),
